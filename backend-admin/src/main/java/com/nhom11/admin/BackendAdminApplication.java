84f98af88f11cab064f9d52658d01b7de85dc959
NguyenVanTrongTri <154603556+NguyenVanTrongTri@users.noreply.github.com>
2026-03-10 14:37:00 +0700

    Add main application class for Spring Boot

--- backend-admin/src/main/java/com/nhom11/admin/BackendAdminApplication.java
+++ backend-admin/src/main/java/com/nhom11/admin/BackendAdminApplication.java
@@ -1,1 +1,11 @@
+package com.nhom11.admin;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAdminApplication.class, args);
+    }
+}
