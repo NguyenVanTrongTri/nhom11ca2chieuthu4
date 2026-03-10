a915c864ab3d69548b56c641efdf835472033144
NguyenVanTrongTri <154603556+NguyenVanTrongTri@users.noreply.github.com>
2026-03-10 15:14:03 +0700

    Update UserRepository.java

--- backend-user/src/main/java/com/nhom11/user/repository/UserRepository.java
+++ backend-user/src/main/java/com/nhom11/user/repository/UserRepository.java
@@ -1,1 +1,7 @@
+package com.nhom11.user.repository;
 
+import com.nhom11.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
