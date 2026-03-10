049f69b04c7cfa127b884bc3a743d3270820dd22
NguyenVanTrongTri <154603556+NguyenVanTrongTri@users.noreply.github.com>
2026-03-10 14:37:45 +0700

    Create UserRepository interface for User entity

--- backend-admin/src/main/java/com/nhom11/admin/repository/UserRepository.java
+++ backend-admin/src/main/java/com/nhom11/admin/repository/UserRepository.java
@@ -1,1 +1,7 @@
+package com.nhom11.admin.repository;
 
+import com.nhom11.admin.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
