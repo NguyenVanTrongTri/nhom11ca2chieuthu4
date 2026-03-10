71c2856f9aeb6401cbea371544338b5731d92a32
NguyenVanTrongTri <154603556+NguyenVanTrongTri@users.noreply.github.com>
2026-03-10 15:12:33 +0700

    Initialize Spring Boot application structure

--- backend-user/src/main/java/com/nhom11/user/BackendUserApplication.java
+++ backend-user/src/main/java/com/nhom11/user/BackendUserApplication.java
@@ -1,1 +1,11 @@
+package com.nhom11.user;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendUserApplication.class, args);
+    }
+}
