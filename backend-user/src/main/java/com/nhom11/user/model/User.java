bba0d34cd75ac575a9ec99502d4f834bc8d6f0e0
NguyenVanTrongTri <154603556+NguyenVanTrongTri@users.noreply.github.com>
2026-03-10 15:13:31 +0700

    Update User.java

--- backend-user/src/main/java/com/nhom11/user/model/User.java
+++ backend-user/src/main/java/com/nhom11/user/model/User.java
@@ -1,1 +1,18 @@
+package com.nhom11.user.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    // Getter và Setter
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
