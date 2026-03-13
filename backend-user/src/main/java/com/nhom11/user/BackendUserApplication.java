6bb17ac735f4c07f80fc4caf70c0cecc3afe3a93
NguyenVanTrongTri <154603556+NguyenVanTrongTri@users.noreply.github.com>
2026-03-13 13:45:40 +0700

    Enable scheduling in BackendUserApplication

--- backend-user/src/main/java/com/nhom11/user/BackendUserApplication.java
+++ backend-user/src/main/java/com/nhom11/user/BackendUserApplication.java
@@ -2,8 +2,10 @@ package com.nhom11.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling; 
 
 @SpringBootApplication
+@EnableScheduling
 public class BackendUserApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendUserApplication.class, args);
