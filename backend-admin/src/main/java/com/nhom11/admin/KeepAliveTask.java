512c5159fd25f5eabfa839055c9726dbdec95e69
NguyenVanTrongTri <154603556+NguyenVanTrongTri@users.noreply.github.com>
2026-03-13 15:25:09 +0700

    Update KeepAliveTask.java

--- backend-admin/src/main/java/com/nhom11/admin/KeepAliveTask.java
+++ backend-admin/src/main/java/com/nhom11/admin/KeepAliveTask.java
@@ -7,28 +7,27 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class KeepAliveTask {
 
+    private final RestTemplate restTemplate = new RestTemplate();
+
     @Scheduled(fixedRate = 600000) // 10 phút
     public void keepAlive() {
 
         String[] urls = {
 
-            "https://backend-admin-909u.onrender.com/users",
+            "https://backend-admin-909u.onrender.com/ping",
             "https://nhom11ca2chieuthu4.onrender.com"
         };
-
-        RestTemplate restTemplate = new RestTemplate();
 
         for (String url : urls) {
             try {
 
                 restTemplate.getForEntity(url, String.class);
 
-                System.out.println(">>> [Admin-Service] Da ping de giu thuc: " + url);
+                System.out.println(">>> [Admin-Service] Ping OK: " + url);
 
             } catch (Exception e) {
 
                 System.err.println(">>> [Admin-Service] Ping that bai: " + url);
-
             }
         }
     }
