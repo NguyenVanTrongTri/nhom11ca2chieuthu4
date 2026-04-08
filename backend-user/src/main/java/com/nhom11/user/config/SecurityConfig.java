a6c7e3ca168e59432ca1e6801b3a3aa9a37ff25c
NguyenVanTrongTri <154603556+NguyenVanTrongTri@users.noreply.github.com>
2026-04-08 13:02:40 +0700

    Update SecurityConfig.java

--- backend-user/src/main/java/com/nhom11/user/config/SecurityConfig.java
+++ backend-user/src/main/java/com/nhom11/user/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.config.Customizer;
+
 @Configuration
 public class SecurityConfig {
     private final CustomUserDetailsService customUserDetailsService;
@@ -49,7 +49,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
