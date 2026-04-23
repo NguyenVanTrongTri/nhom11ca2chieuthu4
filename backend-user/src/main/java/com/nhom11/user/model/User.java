f2aa46fa812ce78090bdecd5248cdb971c15cd85
NguyenVanTrongTri <154603556+NguyenVanTrongTri@users.noreply.github.com>
2026-04-23 10:20:57 +0700

    Update User.java

--- backend-user/src/main/java/com/nhom11/user/model/User.java
+++ backend-user/src/main/java/com/nhom11/user/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
